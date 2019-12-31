337b86454807be4ef1414cb085a6cd58da47d9b5
dony <djdony@mail.ru>
2019-12-31 13:39:10 +0300

    Aircraft Add

--- components/SearchPanel.vue
+++ components/SearchPanel.vue
@@ -1,16 +1,14 @@
 <template lang="pug">
 div
   .search-panel
-    .search-group
+    .search-group 
       .search-item.text 
         v-icon mdi-airplane-takeoff
-        v-autocomplete(v-model='filters.from' :items="fromLocationsList" placeholder='From' solo flat hide-details dense)
-        //- v-icon mdi-chevron-right
+        v-autocomplete(v-model='filters.from' :items ="fromLocationsList" placeholder='From' solo flat hide-details dense)
 
       .search-item.text 
         v-icon mdi-airplane-landing
         v-autocomplete(v-model='filters.to' :items="toLocationsList" placeholder='To' solo flat hide-details dense)
-        //- v-icon mdi-chevron-right
         
       v-select(outlined :items='filtersData.products' item-value='id' item-text='name' v-model='filters.product_id' hide-details placeholder='Category' :menu-props='{offsetY:true}' color='secondary').select-category
 
@@ -35,50 +33,21 @@ div
               :show-current='false'
               :events='datesInRange'
               event-color='#3273C2'
+              @change="fetchAirlines" 
               ).range
               v-spacer
               span(v-text='dateMessage[filters.dates.length]' v-if='filters.dates.length < 2').primary--text.caption.px-2.pb-1
               v-btn(text color="primary" @click="dateMenu = false" v-else) OK
       
-      v-menu(offset-y v-model="eventSizeMenu" content-class='white' transition="scale-transition"  :close-on-content-click="false")
-        template(v-slot:activator="{ on }")
-          v-btn.search-item.button(v-on='on')
-            .text
-              span.label Event size
-              span(v-text='filters.pax').value
-            v-icon mdi-human-male
-        v-card(elevation='0').search-menu.white.secondary--text
-          p.search-menu__title Event size
-          .search-menu__field-group
-            span.group__name Single rooms:
-            .control.plus(@click='decr("sgl")'): v-icon mdi-minus
-            v-text-field(v-model='filters.sgl' @input='recalc()' placeholder="0" solo hide-details flat).group__input
-            .control.plus(@click='incr("sgl")'): v-icon mdi-plus
-          .search-menu__field-group
-            span.group__name Double rooms:
-            .control.plus(@click='decr("dbl")'): v-icon mdi-minus
-            v-text-field(v-model='filters.dbl' @input='recalc()' placeholder="0" solo hide-details flat).group__input
-            .control.plus(@click='incr("dbl")'): v-icon mdi-plus
-          .search-menu__field-group
-            span.group__name Triple rooms:
-            .control.plus(@click='decr("trpl")'): v-icon mdi-minus
-            v-text-field(v-model='filters.trpl' @input='recalc()' placeholder="0" solo hide-details flat).group__input
-            .control.plus(@click='incr("trpl")'): v-icon mdi-plus
-          .search-menu__field-group
-            span.group__name Total Pax:
-            .control.plus(@click='decr("pax")'): v-icon mdi-minus
-            v-text-field(v-model='filters.pax' @input='recalc("pax")' placeholder="0" solo hide-details flat).group__input
-            .control.plus(@click='incr("pax")'): v-icon mdi-plus
-          
-          .layout.mt-3
-            v-spacer
-            v-btn(text color="primary" @click="eventSizeMenu = false") OK
+      .search-item.text
+        v-icon mdi-airplane
+        v-autocomplete(v-model='airline' :items="filters.airlines" placeholder='Aircraft' solo flat hide-details dense)
 
       v-btn.search-item.button.submit(@click="search" color='white--text')
         span.label Search
 
   //- SEARCH PARAMS
-
+  div {{ filters.airlines }}
   .search-params(v-if='searchParams')
     v-spacer
     span.search-params__amount {{ hotelsAmount }}
@@ -115,15 +84,12 @@ export default {
         to: null,
         dates: [],
         nights: 3,
-        // rooms
-        sgl: null,
-        dbl: null,
-        trpl: null,
-        pax: null,
         sortby: 'price_asc',
         product_id: 1,
+         airlines: [],
+        
         // signals that there's a query from search-panel
-        searchPanel: true
+        searchPanel: true,
       },
       dateMessage: ['Choose departure day', 'Choose arrival day', ''],
       sortItems: [
@@ -131,8 +97,8 @@ export default {
         { id: 'price_desc', dir: 'desc', name: 'Price High', icon: 'mdi-sort-descending' }
       ],
       dateMenu: null,
-      eventSizeMenu: null,
       fromMenu: null,
+      //airline: [],
       numberFields: [
         'product_id',
         'from',
@@ -160,12 +126,7 @@ export default {
       ])
 
       this.numberFields.forEach(item => {
-        // if (!Array.isArray(item)) {
         getFilters[item] = Number(getFilters[item])
-        // } else {
-        //   getFilters[item[1]] = Number(getFilters[item[0]])
-        //   delete getFilters[item[0]]
-        // }
       })
 
       getFilters.dates = compact([
@@ -185,21 +146,24 @@ export default {
     }),
     ...mapGetters({
       fromLocationsList: 'filters/fromLocationsList',
-      toLocationsList: 'filters/toLocationsList'
+      toLocationsList: 'filters/toLocationsList',
     }),
+      
     travelRange() {
       if (this.filters.dates.length < 1) return
       let departureDate = this.filters.dates[0]
       let arrivalDate = this.filters.dates[1]
+       this.fetchAirlines()
+     
 
       departureDate = this.$dayjs(departureDate).format('DD.MM')
 
       arrivalDate = arrivalDate
         ? this.$dayjs(arrivalDate).format('DD.MM')
         : '...'
-
       return departureDate + ' - ' + arrivalDate
-    }
+    },
+  
   },
   methods: {
     ...mapMutations({ showSnackbar: 'snackbar/showSnackbar' }),
@@ -216,21 +180,18 @@ export default {
       let { dates } = this.filters
       let range = 3
 
-      /*      if(dates.length == 0){
-        return this.$dayjs(widgetDate).unix() >= this.$dayjs(this.filtersData.minDate).unix() && this.$dayjs(widgetDate).unix() <= this.$dayjs(this.filtersData.maxDate).unix()
-      }*/
 
       if (dates.length == 1) {
         let diff = this.$dayjs(widgetDate).diff(this.$dayjs(dates[0]), 'days')
-        return 0 <= diff && diff <= range //&& this.$dayjs(widgetDate).unix() <= this.$dayjs(this.filtersData.maxDate).unix()
+        return 0 <= diff && diff <= range
       }
       if (dates.length == 3) this.filters.dates = [dates[2]]
 
       if (dates.length > 3) this.filters.dates = []
-      //if (dates.length === 2){ this.dateMenu = false }
 
       return true
     },
+    
     datesInRange(widgetDate) {
       let { dates } = this.filters
 
@@ -239,9 +200,21 @@ export default {
       let minDate = new Date(dates[0])
       let maxDate = new Date(dates[1])
       let toCompareDate = new Date(widgetDate)
+     
 
       return minDate < toCompareDate && toCompareDate < maxDate
     },
+    async fetchAirlines(){
+       
+       if (!this.filters.dates[1]) return
+      
+      let list = await this.$axios.get('/user/filters', { params: {'arrival_date': this.filters.dates[0] }} )
+      console.log(list.data.airline)
+      this.filters.airlines[0][name] = 'Atlas'
+      this.filters.airlines[1][name] = 'Inventum'
+      
+    },
+
     async search() {
       if (!this.validate()) return
 
@@ -256,19 +229,6 @@ export default {
 
       this.$router.push({ path: '/search', query })
       await this.$emit('search')
-    },
-    recalc(e) {
-      if (e == 'pax') {
-        this.filters.sgl = 0
-        this.filters.dbl =
-          this.filters.pax > 0 ? Math.ceil(this.filters.pax / 2) : 0
-        this.filters.trpl = 0
-      } else {
-        this.filters.pax =
-          Number(this.filters.sgl) +
-          Number(this.filters.dbl * 2) +
-          Number(this.filters.trpl * 3)
-      }
     },
     validate() {
       if (!this.filters.from) {
@@ -294,7 +254,7 @@ export default {
     }
   },
   watch: {
-    'filters.departure_date'() {
+   'filters.departure_date'() {
       this.filters.arrival_date = this.$dayjs(this.filters.departure_date)
         .add(this.filters.nights, 'day')
         .format('YYYY-MM-DD')
